fd21dae1126c4a03f8662f2ca75d7763debc894c
Matthew Andrews <Matthew.Andrews.018@gmail.com>
2026-05-12 13:16:00 -0400

    wip

--- tempest/src/models/mod.rs
+++ tempest/src/models/mod.rs
@@ -3,3 +3,4 @@ pub mod directory_model;
 pub mod test_model;
 pub mod options_model;
 pub mod test_result;
+pub mod report_template_model;
---
+++ tempest/src/models/report_template_model.rs
@@ -0,0 +1,4 @@
+
+pub struct ReportTemplateModel{
+    pub test: String,
+}
